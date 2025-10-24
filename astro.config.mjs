f13b0b25cf6386801c7a1139dc68a2976b5773e4
mouro <dolbrebr@gmail.com>
2025-10-24 13:48:43 -0300

    chore: componente menu finalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  server: { open: true },
+  server: { open: true, port: 42069 },
   integrations: [icon()],
 });
